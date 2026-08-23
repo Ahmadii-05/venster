f07d80abd9c2d3dcf126e28dcea174475cf49381
Shivaji Kompelli <shivajikompelli@gmail.com>
2026-08-23 17:42:00 +0530

    Remove dead legacy backend

--- backend/src_backup/src/main/java/com/microhubs/auth/UserRepository.java
+++
@@ -1,9 +0,0 @@
-﻿package com.microhubs.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
-    boolean existsByEmail(String email);
-}
